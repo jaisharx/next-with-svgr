86212aeff5ed217f8ace83ebe3e733c4b2452210
jai-sharma <sendtojsharma@gmail.com>
2021-03-30 09:45:20 +0530

    add svgr webpack loader

---
+++ components/svg/index.js
@@ -0,0 +1,11 @@
+import Vercel from './vercel.svg';
+
+export default function Svg() {
+    return (
+        <>
+            {/* this is an inlined-svg */}
+            <Vercel />
+            {/* this is an inlined-svg */}
+        </>
+    );
+}
